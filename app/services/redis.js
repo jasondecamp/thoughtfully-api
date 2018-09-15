c5afec126aefe812d926e7f2bcab8ba0e1304802
Jason DeCamp <smashingjason@gmail.com>
2018-09-14 23:07:42 -0400

    update redis env key

--- app/services/redis.js
+++ app/services/redis.js
@@ -2,4 +2,4 @@
 
 const redis = require('redis');
 
-module.exports = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+module.exports = redis.createClient(process.env.REDIS_URL, {no_ready_check: true});
